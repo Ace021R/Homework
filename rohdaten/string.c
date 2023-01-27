ffebbfa9612904925d6275388d95998dbd3124c4
Amirhomayoun Rouhnavaz <119923146+Ace021R@users.noreply.github.com>
2023-01-27 15:35:01 +0100

    Update string.c

--- rohdaten/string.c
+++ rohdaten/string.c
@@ -3,7 +3,7 @@
 
 int main () {
 
-	
+	    int i, tag;
 		char * wochentage[7];
 
 		wochentage [0] = "Montag";
@@ -13,13 +13,10 @@ int main () {
 		wochentage [4] = "Freitag";
 		wochentage [5] = "Samstag";
 		wochentage [6] = "Sonntag";
+		
+    printf("Wählen Sie einen Tag aus (0-6): ");
+    scanf("%d", &tag);
 
-
-	int 	
-		printf("Geben Sie bitte ein Zahl ein");
-		scanf("%d", &wochentage);
-
-	
-		printf("Wochentag");
+    printf("Heute ist %s\n", wochentage[tag]);
 }
 
