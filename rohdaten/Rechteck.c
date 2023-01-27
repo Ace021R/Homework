8f9a33318eadb77f98c975d52a378440a9da1c6e
Amirhomayoun Rouhnavaz <119923146+Ace021R@users.noreply.github.com>
2023-01-27 15:15:28 +0100

    Update Rechteck.c

--- rohdaten/Rechteck.c
+++ rohdaten/Rechteck.c
@@ -1,14 +1,10 @@
 #include<stdio.h> 
 int main () {
 
-	int i, j;
+	int i, n;
 	
 	for(i=0; i<=10; i++) {
-
-		for(j=0; j<=10; j++) {
-			printf("*");
-		}
-	printf("\n*");
+        printf("**********\n");
 	}
 }
 
