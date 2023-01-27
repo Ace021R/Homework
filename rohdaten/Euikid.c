fa5d4dd2045fbf01fd87a4131b15407cb0c37b14
Amirhomayoun Rouhnavaz <119923146+Ace021R@users.noreply.github.com>
2023-01-27 11:50:26 +0100

    Update Euikid.c

--- rohdaten/Euikid.c
+++ rohdaten/Euikid.c
@@ -1,8 +1,27 @@
-#include<stdio.h>
-int main () {
+#include <stdio.h>
 
-	int one, two;
+int funk(int zahl1, int zahl2) {
+    if (zahl1 == zahl2) 
+        return zahl1;
+        else if (zahl1 > zahl2)
+            return funk(zahl1-zahl2, zahl1);
+            else {
+                return funk(zahl2, zahl2-zahl1);
+            }
+            }
+       
 
 
+int main() {
+    
+    int zahl1, zahl2, one;
+    
 
+    printf("geben sie das erste Zahl ein:  ");
+    scanf("%d", &zahl1);
+    printf("geben sie das zweite Zahl ein:  ");
+    scanf("%d", &zahl2);
+    printf("%d", funk(zahl1, zahl2));
+    
+    return 0;
 }
